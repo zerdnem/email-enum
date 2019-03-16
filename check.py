b70062993a43c80cf41827713f4c8d34c65cb2cb
Frinto <frinto@protonmail.com>
2019-03-15 19:56:17 -0700

    v1.0 Commit

--- check.py
+++ check.py
@@ -4,18 +4,19 @@ import sys
 import click
 from termcolor import colored
 import subprocess
+import pyfiglet
 
 def asciiBanner():
-    import pyfiglet
     banner = pyfiglet.figlet_format("Email Enum")
     return banner
 
 @click.command()
 @click.argument('email', nargs=1)
-def emailEnum(email):
+@click.option('--output', help='Output results into file')
+def emailEnum(email, output):
     print(colored(asciiBanner(), 'magenta'))
     print(colored("Author: ", 'green') + "Frinto")
-    print(colored("Version: ", 'green') + colored("v0.5", 'yellow'))
+    print(colored("Version: ", 'green') + colored("v1.0", 'yellow'))
     print("\n\n")
     try:
         firefox_check = subprocess.check_output(['which', 'firefox'])
@@ -23,14 +24,37 @@ def emailEnum(email):
         print(colored("Firefox not installed or isn't in PATH, exiting...", 'yellow'))
         sys.exit()
     import sites
+    excl = colored("[!] ", "yellow")
+    excl2 = colored("[*] ", "magenta")
+    print(excl + "Checking Instagram...")
     insta_response = sites.instagramCheck(email)
-    print("[*] Instagram: " + insta_response)
+    print(excl + "Checking Twitter...")
     twit_response = sites.twitterCheck(email)
-    print("[*] Twitter: " + twit_response)
+    print(excl + "Checking Snapchat...")
     snap_response = sites.snapchatCheck(email)
-    print("[*] Snapchat: " + snap_response)
+    print(excl + "Checking Facebook...")
     face_response = sites.facebookCheck(email)
-    print("[*] Facebook: " + face_response)
+    print(excl + "Checking Google/Youtube...")
+    gooyou_response = sites.yougoogleCheck(email)
+    print(excl + "Checking Twitch...")
+    twitch_response = sites.twitchCheck(email) 
     sites.quitSelenium()
+    print("\n\n")
+    results = pyfiglet.figlet_format("Results") 
+    print(colored(results, "green"))
+    print(excl2 + "Instagram: " + insta_response)
+    print(excl2 + "Twitter: " + twit_response)
+    print(excl2 + "Snapchat: " + snap_response)
+    print(excl2 + "Facebook: " + face_response)
+    print(excl2 + "Google/Youtube: " + gooyou_response)
+    print(excl2 + "Twitch: " + twitch_response)
+    if output is not None:
+        outputfile = open(output, "a")
+        listout = ["Instagram: " + insta_response, "Twitter: " + twit_response, "Snapchat: " + snap_response, "Facebook: " + face_response, "Google/Youtube: " + gooyou_response, "Twitch: " + twitch_response]
+        outputfile.writelines((i + '\n' for i in listout))
+        outputfile.close()
+        sys.exit()
+    else:
+        sys.exit()
 
 emailEnum()
--- sites.py
+++ sites.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotInteractableException
 
 options = webdriver.FirefoxOptions()
 options.add_argument("--headless")
@@ -18,7 +20,7 @@ def instagramCheck(email):
     user.send_keys(Keys.RETURN)
     #driver.find_element_by_tag_name("button").click()
     sleep(1)
-    if "Thanks! Please check" or "We sent an email to" in driver.page_source:
+    if "Thanks! Please check" in driver.page_source:
         result = "Found"
     else:
         result = "Not Found"
@@ -81,3 +83,45 @@ def facebookCheck(email):
     else:
         result = "Captcha encountered, you'll have to check this manually"
         return result
+
+def yougoogleCheck(email):
+    driver.get("https://accounts.google.com/signin/v2/identifier?hl=en&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
+    assert "Sign in" in driver.title
+    sleep(1)
+    user = driver.find_element_by_name("identifier")
+    user.send_keys(email)
+    user.send_keys(Keys.ENTER)
+    sleep(1.5)
+    try:
+        password  = driver.find_element_by_name("password")
+        result = "Found"
+        return result
+    except NoSuchElementException as notfound:
+        try:
+            captcha_enter = driver.find_element_by_name("ca")
+            captcha_enter.send_keys("test")
+            result = "Captcha encountered, you'll have to check this manually"
+            return result
+        except ElementNotInteractableException as notfoundcaptcha:
+            result = "Not Found"
+            return result
+
+def twitchCheck(email):
+    driver.get("https://www.twitch.tv/")
+    assert "Discover" in driver.page_source
+    driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/nav/div/div[5]/div/div[1]/button").click()
+    sleep(1)
+    user = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[1]/div/div/form/div/div[1]/div/div[2]/input")
+    password = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[1]/div/div/form/div/div[2]/div/div[1]/div[2]/div[1]/input")
+    user.send_keys(email)
+    password.send_keys("123456789")
+    password.send_keys(Keys.ENTER)
+    sleep(1.5)
+    if "This email is not linked to a Twitch account. Please log in with a username instead." in driver.page_source:
+        result = "Not Found"
+        return result
+    else:
+        result = "Found"
+        return result
+
+
